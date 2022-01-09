f506a70eb4528dc5a7f9fd271a3a330bd99510f2
descalsjosma <descalsjosma@disroot.org>
2022-01-09 19:43:02 +0100

    no message

--- sketch_Webserver/sketch_Webserver.ino
+++ sketch_Webserver/sketch_Webserver.ino
@@ -2,7 +2,8 @@
 #include <Ethernet.h>
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 177);
+//IPAddress ip(192, 168, 1, 177);
+IPAddress ip(172, 28, 250, 35);
 EthernetServer server(80);
 
 void setup()
